e0d93d1b3b37fa40bde4caaf47e3d36684960e0a
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-09-24 13:04:43 +0300

    revert to auto-generated service-worker

---
+++ public/precache-manifest.e8b723ee112168dab3dcd1602520b30b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a34ffd6471b42169076",
+    "url": "/css/app.365cbff1.css"
+  },
+  {
+    "revision": "ad1875518dc96cd41c6e733d3a693dd1",
+    "url": "/img/Back and Biceps.ad187551.jpg"
+  },
+  {
+    "revision": "c3fb70537f009f5361462c876646aa6d",
+    "url": "/img/Chest and Triceps.c3fb7053.jpg"
+  },
+  {
+    "revision": "74ad935aa03477d21f51ecf283e93191",
+    "url": "/img/Legs and Shoulders.74ad935a.jpg"
+  },
+  {
+    "revision": "4993502ae4ee7efb8777983773d694bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a34ffd6471b42169076",
+    "url": "/js/app.ed1f3f0a.js"
+  },
+  {
+    "revision": "2fc8ec6734d4e74c66e9",
+    "url": "/js/chunk-121d90a2.c2a3bc41.js"
+  },
+  {
+    "revision": "984aed9314cbd84ea123",
+    "url": "/js/chunk-562965d2.7eec99b9.js"
+  },
+  {
+    "revision": "8c97e7174cfcdf708028",
+    "url": "/js/chunk-vendors.75167c26.js"
+  },
+  {
+    "revision": "524986b270d3bc651dd2ef46b2c40cd9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,42 +1,34 @@
-importScripts("/precache-manifest.c66091a3da62d6146f00f76a620281ad.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-importScripts('/workbox-v4.3.1/workbox-sw.js')
-workbox.setConfig({ debug: false })
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-workbox.core.setCacheNameDetails({
-	prefix: 'client',
-})
+importScripts(
+  "/precache-manifest.e8b723ee112168dab3dcd1602520b30b.js"
+);
 
-// self.addEventListener('fetch', function(event) {
-// 	event.respondWith(
-// 		fetch(event.request).catch(function() {
-// 			return caches.match(event.request)
-// 		})
-// 	)
-// })
+workbox.core.setCacheNameDetails({prefix: "client"});
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.open('client').then(function(cache) {
-			return cache.match(event.request).then(function(response) {
-				return (
-					response ||
-					fetch(event.request).then(function(response) {
-						cache.put(event.request, response.clone())
-						return response
-					})
-				)
-			})
-		})
-	)
-})
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
-self.addEventListener('message', event => {
-	if (event.data && event.data.type === 'SKIP_WAITING') {
-		self.skipWaiting()
-	}
-})
-
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
-
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
