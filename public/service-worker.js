884d40ad28607b44c850736b64c8caec571888a8
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-09-24 12:53:33 +0300

    test manual service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,35 +1,42 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts("/precache-manifest.53e889e3da8d50eacd206cd39b31459d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts("/workbox-v4.3.1/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+importScripts('/workbox-v4.3.1/workbox-sw.js')
+workbox.setConfig({ debug: false })
 
-importScripts(
-  "/precache-manifest.e005f4861bd0a3524702506c459eb902.js"
-);
+workbox.core.setCacheNameDetails({
+	prefix: 'client',
+})
 
-workbox.core.setCacheNameDetails({prefix: "client"});
+// self.addEventListener('fetch', function(event) {
+// 	event.respondWith(
+// 		fetch(event.request).catch(function() {
+// 			return caches.match(event.request)
+// 		})
+// 	)
+// })
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.open('client').then(function(cache) {
+			return cache.match(event.request).then(function(response) {
+				return (
+					response ||
+					fetch(event.request).then(function(response) {
+						cache.put(event.request, response.clone())
+						return response
+					})
+				)
+			})
+		})
+	)
+})
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.addEventListener('message', event => {
+	if (event.data && event.data.type === 'SKIP_WAITING') {
+		self.skipWaiting()
+	}
+})
+
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
